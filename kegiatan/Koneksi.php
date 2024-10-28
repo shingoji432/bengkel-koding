8e2855092f25e58b3d7b8e7f06e1fc02dbb185e3
MuhammadIqbal432 <105849177+shingoji432@users.noreply.github.com>
2024-10-28 13:56:02 +0700

    Add files via upload

---
+++ kegiatan/Koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = '127.0.0.1';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli= mysqli_connect($databaseHost,$databaseUsername,$databasePassword, $databaseName);
